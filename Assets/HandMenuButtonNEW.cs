76c76f1496931c8c47b98999f3e1420f44e432ff
Tri B <triminhbui@csu.fullerton.edu>
2024-12-03 15:33:05 -0800

    Made new hand menu button prefab that hides and shows label when user points at it

---
+++ Assets/HandMenuButtonNEW.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HandMenuButton : MonoBehaviour
+{
+    public GameObject label;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        label.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnHover(BaseEventData eventData)
+    {
+        Debug.Log("Hovering");
+        label.SetActive(true);
+    }
+
+    public void OnHoverEnd(BaseEventData eventData)
+    {
+        Debug.Log("Hover End");
+        label.SetActive(false);
+    }
+}
